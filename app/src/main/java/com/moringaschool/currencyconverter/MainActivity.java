08cb3629cbc38c5eef7a1adfbe30f720120f3f83
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-02-03 06:32:48 +0300

    implemented a splash screen

--- app/src/main/java/com/moringaschool/currencyconverter/MainActivity.java
+++ app/src/main/java/com/moringaschool/currencyconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        };thread.start();
 
 
     }
